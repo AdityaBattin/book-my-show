ecb6ae9f68dc81e6b04bcee89f5f28ceba79b4bf
AdityaBattin <aditya.g.battin@gmail.com>
2022-09-28 23:00:31 +0530

    updated

--- src/Component/Entertainment/Entertainment.Card.Component.jsx
+++ src/Component/Entertainment/Entertainment.Card.Component.jsx
@@ -1,7 +1,34 @@
 import React from "react";
+import { Slider } from "react-slick";
 
-const EntertainmentCard = () => {
-  return <div>EntertainmentCard</div>;
+const EntertainmentCard = (props) => {
+  return (
+    <>
+      <div className="w-full h-30 px-2">
+        <img
+          className="w-full h-full rounded-lg"
+          src={props.src}
+          alt="card-slider"
+        />
+      </div>
+    </>
+  );
+};
+
+// creating card slider
+const EntertainmentCardSlider = () => {
+  const EntertainmentImage = [];
+
+  const settingSlider = {};
+  return (
+    <>
+      <Slider {...settings}>
+        {EntertainmentImage.map((image) => {
+          <EntertainmentCard src={image}></EntertainmentCard>;
+        })}
+      </Slider>
+    </>
+  );
 };
 
 export default EntertainmentCard;
